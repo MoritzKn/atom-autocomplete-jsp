9f4973cb9a688c1b611895472c7285b68aa90896
MoritzKn <moritz.kneilmann@gmx.de>
2016-12-01 16:09:05 +0100

    Load tld files completely async

--- src/sources/tlds.js
+++ src/sources/tlds.js
@@ -6,46 +6,53 @@ import {add as addToRegistry} from '../registry';
 
 
 export function register() {
-    const userHome = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+    const userHome = process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'];
+    const tldSources = atom.config.get('autocomplete-jsp.tldSources');
 
-    let tldPathes = [];
     // TODO: refresh on config change
-    atom.config.get('autocomplete-jsp.tldSources').forEach(dir => {
-            dir = dir.replace('~', userHome);
+    Promise.all(tldSources
+            .map(path => path = path.replace('~', userHome))
+            .map(path => new Promise((resolve, reject) =>
+                fs.readdir(path, (err, fileNames) => {
+                    if (err) {
+                        return reject(err);
+                    }
 
-            fs.readdirSync(dir)
-                .filter(fileName => fileName.endsWith('.tld'))
-                .forEach(fileName => {
-                    const path = `${dir.replace(/\/$/, '')}/${fileName}`;
-                    tldPathes.push(path);
-                });
-        });
 
-    // TODO: when a tld changes, we have to some how reload it..
-    Promise.all(tldPathes.map(readInTld))
-        .then(tldDescs => {
-            tldDescs.forEach(tldDesc => {
-                tldDesc.functions.forEach(fnDesc => {
+                    resolve(fileNames
+                        .filter(name => name.endsWith('.tld'))
+                        .map(name => `${path.replace(/\/$/, '')}/${name}`)
+                    );
+                })
+            ))
+        )
+        // TODO: reload on change
+        .then(result => Promise.all(result
+                .reduce((all, next) => all.concat(next), [])
+                .map(readInTld)
+            )
+        )
+        .then(tldDescs => tldDescs
+            .forEach(tldDesc => {
+                tldDesc.functions.forEach(fnDesc =>
                     addToRegistry({
                         element: fnDesc,
-                        // TODO: not Infinity
                         liveTime: Infinity,
-                    });
-                });
+                    })
+                );
 
-                tldDesc.tags.forEach(tagDesc => {
+                tldDesc.tags.forEach(tagDesc =>
                     addToRegistry({
                         element: tagDesc,
-                        // TODO: not Infinity
                         liveTime: Infinity,
-                    });
-                });
-            });
-        })
-        .catch(err => {
+                    })
+                );
+            })
+        )
+        .catch(err =>
             atom.notifications.addWarning(err.msg, {
                 dismissable: true,
                 detail: `Caused by:\n${err.causedBy}`,
-            });
-        });
+            })
+        );
 }
