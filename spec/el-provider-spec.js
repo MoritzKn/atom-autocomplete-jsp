ad99cefe253ed9aad895f6817df30658e4c02682
MoritzKn <moritz.kneilmann@gmx.de>
2017-01-26 14:29:09 +0100

    Refactor EL provider spec

--- spec/el-provider-spec.js
+++ spec/el-provider-spec.js
@@ -29,8 +29,8 @@ describe('JSP autocompletions EL provider', () => {
     function getCompletion(filter, activatedManually=false) {
         const completions = getCompletions(activatedManually);
 
-        const matchingCompletions = completions.filter(comp =>
-            (comp.text || comp.snippet).includes(filter));
+        const matchingCompletions = completions
+            .filter(comp => (comp.text || comp.snippet).includes(filter));
 
         return matchingCompletions[0];
     }
@@ -83,8 +83,7 @@ describe('JSP autocompletions EL provider', () => {
         atom.config.set('autocomplete-plus.minimumWordLength', 3);
     });
 
-
-    it('returns no completions when inside an empty expression', () => {
+    it('returns no completions inside empty expressions', () => {
         setContent('${', '}');
         const completions = getCompletions();
 
@@ -92,165 +91,216 @@ describe('JSP autocompletions EL provider', () => {
         expect(completions.length).toBe(0);
     });
 
-    it('returns completions inside an empty expression when activated manually', () => {
+    it('returns completions inside empty expressions when activated manually', () => {
         setContent('${a', '}');
         const completions = getCompletions(true);
 
         expect(completions.length).toBeGreaterThan(0);
     });
 
-    it('returns completions for `not` keyword', () => {
-        setContent('${not', '}');
-        const completion = getCompletion('not', true);
-
-        expect(completion).toBeDefined();
-        if (completion) {
-            expect(completion.leftLabel).toBeUndefined();
-            expect(completion.description).toBeDefined();
-            expect(completion.type).toBe('keyword');
-            expect(completion.replacementPrefix).toBe('not');
-        }
-    });
-
-    it('returns completions for `ne` keyword', () => {
-        setContent('${foo ne', '}');
-        const completion = getCompletion('ne', true);
-
-        expect(completion).toBeDefined();
-        if (completion) {
-            expect(completion.leftLabel).toBeUndefined();
-            expect(completion.rightLabel).toBe('not equal');
-            expect(completion.description).toBeDefined();
-            expect(completion.type).toBe('keyword');
-            expect(completion.replacementPrefix).toBe('ne');
-        }
-    });
-
-    it('returns completions for the implicit object `initParam`', () => {
-        setContent('${initPa', '}');
-        const completion = getCompletion('initParam', true);
-
-        expect(completion).toBeDefined();
-        if (completion) {
-            expect(completion.leftLabel).toBe('Map');
-            expect(completion.description).toBeDefined();
-            expect(completion.type).toBe('variable');
-            expect(completion.replacementPrefix).toBe('initPa');
-        }
-    });
-
-    it('returns completions `initParam` for the abbreviation `ip`', () => {
-        setContent('${ip', '}');
-        const completion = getCompletion('initParam', true);
-
-        expect(completion).toBeDefined();
-    });
-
-    it('returns completions for variables defined in `<c:set var="fooBarBaz">`', () => {
-        const text = '<c:set var="fooBarBaz">\n' +
-            '${fooBa';
-        setContent(text, '}');
-        waitsFor(() => varInReg('fooBarBaz'), 3000);
-        runs(() => {
-            const completion = getCompletion('fooBarBaz', true);
-
-            expect(completion).toBeDefined();
-            if (completion) {
-                expect(completion.leftLabel).toBe('');
-                expect(completion.type).toBe('variable');
-                expect(completion.description).toBe('');
-                expect(completion.replacementPrefix).toBe('fooBa');
-            }
-        });
-    });
-
-
-    it('returns completions for references from `<jsp:useBean>`', () => {
-        setContent('<jsp:useBean id="myMap" class="java.utils.HashMap">\n${myMa', '}');
-        waitsFor(() => varInReg('myMap'), 3000);
-        runs(() => {
-            const completion = getCompletion('myMap', true);
-
-            expect(completion).toBeDefined();
-            if (completion) {
-                expect(completion.leftLabel).toBe('HashMap');
-                expect(completion.type).toBe('variable');
-                expect(completion.description).toBe('');
-                expect(completion.replacementPrefix).toBe('myMa');
-            }
-        });
-    });
-
-    it('returns no completions from `.tld` files if they are not imported', () => {
-        loadTestTld();
-        runs(() => {
-            setContent('${ts:', '}');
-            const completion = getCompletion('ts:concat', true);
-
-            expect(completion).toBeUndefined();
-        });
-    });
-
-    it('returns completions from `.tld` files if imported with taglib directive', () => {
-        loadTestTld();
-        runs(() => {
-            const text =  '<%@ taglib\n' +
-                'uri="http://example.com/jsp/test" prefix="prefixOfTag" %>\n' +
-                '\n' +
-                '${prefixOfTag:';
-            setContent(text, '}');
-            const completion = getCompletion('prefixOfTag:concat', true);
-
-            expect(completion).toBeDefined();
-            if (completion) {
-                expect(completion.description).toBe('Concatenates two strings.');
-                expect(completion.leftLabel).toBe('String');
-                expect(completion.type).toBe('function');
-                expect(completion.replacementPrefix).toBe('prefixOfTag:');
-            }
-        });
-    });
-
-    it('returns completions from `.tld` files if imported with xml taglib directive', () => {
-        loadTestTld();
-        runs(() => {
-            const text = '<jsp:directive.taglib ' +
-                'uri="http://example.com/jsp/test" ' +
-                'prefix="prefixOfTag" />\n' +
-                '\n' +
-                '${prefixOfTag:';
-            setContent(text, '}');
-            runs(() => {
-                const completion = getCompletion('prefixOfTag:concat', true);
+    describe('completions for keyword', () => {
+        ['div', 'mod', 'eq', 'ne', 'lt', 'gt', 'le', 'ge', 'and', 'or', 'not', 'empty'].forEach(keyword => {
+            it(`returns a completion for ${keyword} keyword'`, () => {
+                setContent('${foo ' + keyword, '}');
+                const completion = getCompletion(keyword, true);
 
                 expect(completion).toBeDefined();
                 if (completion) {
-                    expect(completion.description).toBe('Concatenates two strings.');
-                    expect(completion.leftLabel).toBe('String');
-                    expect(completion.type).toBe('function');
-                    expect(completion.replacementPrefix).toBe('prefixOfTag:');
+                    expect(completion.leftLabel).toBeUndefined();
+                    expect(completion.description).toBeDefined();
+                    expect(completion.type).toBe('keyword');
+                    expect(completion.replacementPrefix).toBe(keyword);
+                }
+            });
+        });
+
+        it(`is case insensitive'`, () => {
+            setContent('${foo nOt', '}');
+            const completion = getCompletion('not', true);
+
+            expect(completion).toBeDefined();
+        });
+    });
+
+    describe('completions for implicit object', () => {
+        it('returns a completion for pageContext', () => {
+            setContent('${pageCont', '}');
+            const completion = getCompletion('pageContext', true);
+
+            expect(completion).toBeDefined();
+            if (completion) {
+                expect(completion.leftLabel).toBe('PageContext');
+                expect(completion.description).toBeDefined();
+                expect(completion.type).toBe('variable');
+                expect(completion.replacementPrefix).toBe('pageCont');
+            }
+        });
+
+        it('returns a completion for param', () => {
+            setContent('${para', '}');
+            const completion = getCompletion('param', true);
+
+            expect(completion).toBeDefined();
+            if (completion) {
+                expect(completion.leftLabel).toBe('Map');
+                expect(completion.description).toBeDefined();
+                expect(completion.type).toBe('variable');
+                expect(completion.replacementPrefix).toBe('para');
+            }
+        });
+
+        it('returns a completion for initParam', () => {
+            setContent('${initPa', '}');
+            const completion = getCompletion('initParam', true);
+
+            expect(completion).toBeDefined();
+            if (completion) {
+                expect(completion.leftLabel).toBe('Map');
+                expect(completion.description).toBeDefined();
+                expect(completion.type).toBe('variable');
+                expect(completion.replacementPrefix).toBe('initPa');
+            }
+        });
+
+        it('returns a completion for headerValues', () => {
+            setContent('${headerVal', '}');
+            const completion = getCompletion('headerValues', true);
+
+            expect(completion).toBeDefined();
+            if (completion) {
+                expect(completion.leftLabel).toBe('Map');
+                expect(completion.description).toBeDefined();
+                expect(completion.type).toBe('variable');
+                expect(completion.replacementPrefix).toBe('headerVal');
+            }
+        });
+
+        it(`is case insensitive'`, () => {
+            setContent('${foo pAgeCOnT', '}');
+            const completion = getCompletion('pageContext', true);
+
+            expect(completion).toBeDefined();
+        });
+
+        it(`it supports abbreviations'`, () => {
+            setContent('${foo ip', '}');
+            const completion = getCompletion('initParam', true);
+
+            expect(completion).toBeDefined();
+        });
+    });
+
+    describe('completions for variables defined in tags', () => {
+        it('returns completions for variables defined in `<c:set>` tags', () => {
+            setContent('<c:set var="fooBarBaz">\n${fooBa', '}');
+            waitsFor(() => varInReg('fooBarBaz'), 3000);
+            runs(() => {
+                const completion = getCompletion('fooBarBaz');
+
+                expect(completion).toBeDefined();
+                if (completion) {
+                    expect(completion.leftLabel).not.toBeTruthy();
+                    expect(completion.type).toBe('variable');
+                    expect(completion.description).not.toBeTruthy();
+                    expect(completion.replacementPrefix).toBe('fooBa');
+                }
+            });
+        });
+
+        it('returns completions for variables defined in `<jsp:useBean>`', () => {
+            setContent('<jsp:useBean id="myMap" class="java.utils.HashMap">\n${myMa', '}');
+            waitsFor(() => varInReg('myMap'), 3000);
+            runs(() => {
+                const completion = getCompletion('myMap');
+
+                expect(completion).toBeDefined();
+                if (completion) {
+                    expect(completion.leftLabel).toBe('HashMap');
+                    expect(completion.type).toBe('variable');
+                    expect(completion.description).not.toBeTruthy();
+                    expect(completion.replacementPrefix).toBe('myMa');
                 }
             });
         });
     });
 
-    it('returns completions from `.tld` files if imported as xml namespace', () => {
-        loadTestTld();
-            runs(() => {
-            const text = '<jsp:root xmlns:prefixOfTag="http://example.com/jsp/test">\n' +
-                '\n' +
-                '${prefixOfTag:';
-            setContent(text, '}');
-            runs(() => {
-                const completion = getCompletion('prefixOfTag:concat', true);
+    describe('taglibs handling', () => {
+        const importTaglibStr = '<%@ taglib\nuri="http://example.com/jsp/test" prefix="prefixOfTag" %>\n';
 
-                expect(completion).toBeDefined();
-                if (completion) {
-                    expect(completion.description).toBe('Concatenates two strings.');
-                    expect(completion.leftLabel).toBe('String');
-                    expect(completion.type).toBe('function');
-                    expect(completion.replacementPrefix).toBe('prefixOfTag:');
-                }
+        it('returns no completions from not imported taglibs', () => {
+            loadTestTld();
+            runs(() => {
+                setContent('${ts:', '}');
+                const completions = getCompletions();
+
+                expect(completions.length).toBe(0);
+            });
+        });
+
+        describe('taglibs imports', () => {
+            it('returns completions from taglibs imported by taglib directives', () => {
+                loadTestTld();
+                runs(() => {
+                    const text =  '<%@ taglib\n' +
+                        'uri="http://example.com/jsp/test" prefix="prefixOfTag" %>\n' +
+                        '${prefixOfTag:';
+                    setContent(text, '}');
+                    expect(getCompletion('prefixOfTag')).toBeDefined();
+                });
+            });
+
+            it('returns completions from taglibs imported by XML taglib directives', () => {
+                loadTestTld();
+                runs(() => {
+                    const text = '<jsp:directive.taglib\n' +
+                        'uri="http://example.com/jsp/test" prefix="prefixOfTag"/>\n' +
+                        '${prefixOfTag:';
+                    setContent(text, '}');
+                    expect(getCompletion('prefixOfTag')).toBeDefined();
+                });
+            });
+
+            it('returns completions from taglibs imported by XML namespace attributes', () => {
+                loadTestTld();
+                runs(() => {
+                    const text = '<someTag xmlns:prefixOfTag="http://example.com/jsp/test"/>\n' +
+                        '${prefixOfTag:';
+                    setContent(text, '}');
+                    expect(getCompletion('prefixOfTag')).toBeDefined();
+                });
+            });
+        });
+
+        describe('completions for functions defined in imported taglibs', () => {
+            it('is case insensitive', () => {
+                setContent(importTaglibStr + '${prEfIxOfTag:cOn', '}');
+                expect(getCompletion('prefixOfTag:concat')).toBeDefined();
+            });
+
+            it('contains the description from the TLD file', () => {
+                setContent(importTaglibStr + '${prefixOfTag:con', '}');
+                expect(getCompletion('prefixOfTag:concat').description).toBe('Concatenates two strings.');
+            });
+
+            it('contains the type from the TLD file', () => {
+                setContent(importTaglibStr + '${prefixOfTag:con', '}');
+                expect(getCompletion('prefixOfTag:concat').leftLabel).toBe('String');
+            });
+
+            it('is of type function', () => {
+                setContent(importTaglibStr + '${prefixOfTag:con', '}');
+                expect(getCompletion('prefixOfTag:concat').type).toBe('function');
+            });
+
+            it('replaces only the namespace and the function name', () => {
+                setContent(importTaglibStr + '${prefixOfTag:con', '}');
+                expect(getCompletion('prefixOfTag:concat').replacementPrefix).toBe('prefixOfTag:con');
+
+                setContent(importTaglibStr + '${test:foo(prefixOfTag:con', ')}');
+                expect(getCompletion('prefixOfTag:concat').replacementPrefix).toBe('prefixOfTag:con');
+
+                setContent(importTaglibStr + '${prefix}');
+                expect(getCompletion('prefixOfTag:concat').replacementPrefix).toBe('prefix');
             });
         });
     });
