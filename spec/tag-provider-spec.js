8892187338962301c8633a6906fa8877989e2b0c
MoritzKn <moritz.kneilmann@gmx.de>
2017-02-09 11:53:04 +0100

    Add specs for attribute completion

--- spec/tag-provider-spec.js
+++ spec/tag-provider-spec.js
@@ -56,17 +56,10 @@ describe('JSP autocompletions tag provider', () => {
     });
 
     describe('tag completion', () => {
-        it('returns no completions after `<`', () => {
-            setContent('<');
-            const completions = getCompletions();
-
-            expect(completions.length).toBe(0);
-        });
-
         it('returns completions after `<` when activated manually', () => {
             setContent(importTaglibStr + '<');
             const completions = getCompletions(true);
-            console.log(completions.map(t => t.snippet));
+
             expect(completions.length).toBeGreaterThan(0);
         });
 
@@ -105,6 +98,28 @@ describe('JSP autocompletions tag provider', () => {
             expect(completion.description).toBe('Tag for iteration.');
         });
 
+        it('only closes the tag for closing tags', () => {
+            setContent(importTaglibStr + '</prefixOfTag:for');
+            const completion = getCompletion('prefixOfTag:forEach');
+
+            expect(completion).toBeDefined();
+            expect(completion.snippet).toBe('prefixOfTag:forEach>');
+        });
+
+        it('only completes the name for already closed tags', () => {
+            setContent(importTaglibStr + '<prefixOfTag:for', '>');
+            const completion = getCompletion('prefixOfTag:forEach');
+
+            expect(completion).toBeDefined();
+            expect(completion.snippet).toBe('prefixOfTag:forEach');
+
+            setContent(importTaglibStr + '<prefixOfTag:for', 'foo="bar" >');
+            const completion2 = getCompletion('prefixOfTag:forEach');
+
+            expect(completion2).toBeDefined();
+            expect(completion2.snippet).toBe('prefixOfTag:forEach');
+        });
+
         it('adds all required attributes', () => {
             setContent(importTaglibStr + '<forEach');
             const completion = getCompletion('prefixOfTag:forEach');
@@ -113,7 +128,7 @@ describe('JSP autocompletions tag provider', () => {
             expect(completion.snippet).toContain('requiredTest="');
         });
 
-        it('adds no attributes that is not required', () => {
+        it('adds no not required attribute', () => {
             setContent(importTaglibStr + '<forEach');
             const completion = getCompletion('prefixOfTag:forEach');
 
@@ -121,7 +136,7 @@ describe('JSP autocompletions tag provider', () => {
             expect(completion.snippet).not.toContain('items="');
         });
 
-        it(`is case insensitive'`, () => {
+        it('is case insensitive', () => {
             setContent(importTaglibStr + '<pReFiXofTAg:fOre');
             const completion = getCompletion('forEach');
 
@@ -129,7 +144,7 @@ describe('JSP autocompletions tag provider', () => {
             expect(completion.replacementPrefix).toBe('pReFiXofTAg:fOre');
         });
 
-        it(`supports abbreviations'`, () => {
+        it('supports abbreviations', () => {
             setContent(importTaglibStr + '<fe');
             const completion = getCompletion('forEach');
 
@@ -138,11 +153,84 @@ describe('JSP autocompletions tag provider', () => {
         });
     });
 
-    describe('tag attribute completion', () => {
-        // TODO:
+    describe('attribute completion', () => {
+        it('returns no completions after white space', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach ');
+            const completions = getCompletions();
+
+            expect(completions.length).toBe(0);
+        });
+
+        it('returns completions after attribute prefix', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach ite');
+            const completions = getCompletions(true);
+
+            expect(completions.length).toBeGreaterThan(0);
+        });
+
+        it('shows the correct information', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach ite');
+            const completion = getCompletion('ite');
+
+            expect(completion).toBeDefined();
+            expect(completion.snippet).toContain('items');
+            expect(completion.displayText).toBe('items');
+            expect(completion.type).toBe('attribute');
+            expect(completion.description).toBe('Items to iterate over.');
+        });
+
+        it('doesn\'t complete closing tags', () => {
+            setContent(importTaglibStr + '</prefixOfTag:forEach ite');
+            const completions = getCompletions(true);
+
+            expect(completions.length).toBe(0);
+        });
+
+        it('completes no attributes that are not already present', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach it', ' >');
+            expect(getCompletion('items')).toBeDefined();
+
+            setContent(importTaglibStr + '<prefixOfTag:forEach items="${foo}" it', ' >');
+            expect(getCompletion('items')).toBeUndefined();
+
+            setContent(importTaglibStr + '<prefixOfTag:forEach it', ' items="${foo}">');
+            expect(getCompletion('items')).toBeUndefined();
+        });
+
+        it('adds no attribute value if the attribute has no rtexprvalue', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach va');
+            const completion = getCompletion('var');
+
+            expect(completion).toBeDefined();
+            expect(completion.snippet).toBe('var="$1"');
+        });
+
+        it('adds the type of the attribute value in an EL expressions', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach beg');
+            const completion = getCompletion('begin');
+
+            expect(completion).toBeDefined();
+            expect(completion.snippet).toBe('begin="${${1:int}}"');
+        });
+
+        it('is case insensitive', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach itEm');
+            const completion = getCompletion('items');
+
+            expect(completion).toBeDefined();
+            expect(completion.replacementPrefix).toBe('itEm');
+        });
+
+        it('supports abbreviations', () => {
+            setContent(importTaglibStr + '<prefixOfTag:forEach vs');
+            const completion = getCompletion('varStatus');
+
+            expect(completion).toBeDefined();
+            expect(completion.replacementPrefix).toBe('vs');
+        });
     });
 
-    describe('tag attribute value completion', () => {
+    describe('attribute value completion', () => {
         // TODO:
     });
 });
